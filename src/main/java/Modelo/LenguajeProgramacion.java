0384090adbaaaf73400f1f8cb85c15d422779ed1
zamorafelipe <duglaszamora2004@gmai.com>
2024-11-24 10:48:19 -0500

    Se agregaron modificaciones(atributos y metodos constructor y tostring) a la clase LenguajeProgramacio

--- src/main/java/Modelo/LenguajeProgramacion.java
+++ src/main/java/Modelo/LenguajeProgramacion.java
@@ -1,5 +1,56 @@
 package Modelo;
 
 public class LenguajeProgramacion {
-    
+    private int anioCreacion;
+    private String caracteristicaPrincipal;
+    private String nombre;
+    private String utilizacion;
+
+    // Constructor
+    public LenguajeProgramacion(int anioCreacion, String caracteristicaPrincipal, String nombre, String utilizacion) {
+        this.anioCreacion = anioCreacion;
+        this.caracteristicaPrincipal = caracteristicaPrincipal;
+        this.nombre = nombre;
+        this.utilizacion = utilizacion;
+    }
+
+    // Getters
+    public int getAnioCreacion() {
+        return anioCreacion;
+    }
+
+    public String getCaracteristicaPrincipal() {
+        return caracteristicaPrincipal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUtilizacion() {
+        return utilizacion;
+    }
+
+    // Setters
+    public void setAnioCreacion(int anioCreacion) {
+        this.anioCreacion = anioCreacion;
+    }
+
+    public void setCaracteristicaPrincipal(String caracteristicaPrincipal) {
+        this.caracteristicaPrincipal = caracteristicaPrincipal;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUtilizacion(String utilizacion) {
+        this.utilizacion = utilizacion;
+    }
+
+    // Método ToString
+    @Override
+    public String toString() {
+        return "LenguajeProgramacion{" + "anioCreacion=" + anioCreacion + ", caracteristicaPrincipal='" + caracteristicaPrincipal + '\'' + ", nombre='" + nombre + '\'' + ", utilizacion='" + utilizacion + '\'' + '}';
+    }
 }
